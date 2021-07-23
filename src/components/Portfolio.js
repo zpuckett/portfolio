3331bf3fb5bcc8c63f9e433f034a48618b123a69
Music <Music@Kates-MacBook-Pro.local>
2021-07-23 14:17:42 -0500

    wip

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -14,7 +14,7 @@ export default class Porfolio extends Component {
               return(
                 <div className="columns portfolio-item">
                   <div className="item-wrap">
-                    <td onClick={()=> window.open(`${item.url}, "_blank"`)}>Project Page
+                    <td onClick={()=> window.open(`${item.url}`)}>Project Page
                       <img src={`${item.imgurl}`} className="item-img"/>
                       <div className="overlay">
                         <div className="portfolio-item-meta">
